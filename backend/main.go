6bae569a9f108bc057d10fe02a260d4b38164420
Rogerio <rogeriopiatek@gmail.com>
2024-04-25 21:36:13 -0300

    Initial commit 🚀

---
+++ backend/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/lib/pq"
+)
+
+type User struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+	Email string `json:"email"`
+} 
